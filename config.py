3bb88b514fc6a9580a42ed085ac23fe5fce6ba31
jje041 <jje041@ifilab113-lnx.stud.cs.uit.no>
2022-03-29 12:41:58 +0200

    added Mr. Rime as an exception

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ score = 0
 generations = []
 correct = {}
 valid = {}
-special_cases = ['Type: Null','Tapu Koko','Tapu Lele','Tapu Bulu','Tapu Fini','Mr. Mime','Mime Jr.', 'Nidoran M','Nidoran F']
+special_cases = ['Type: Null','Tapu Koko','Tapu Lele','Tapu Bulu','Tapu Fini','Mr. Mime','Mime Jr.', 'Nidoran M','Nidoran F','Mr. Rime']
 load = False
