40a497a3b49c4d5b081f6575e067904e6ddf11e1
jrn <jje041@post.uit.no>
2024-06-05 12:29:13 +0200

    Removed old version.

--- config.py
+++
@@ -1,6 +0,0 @@
-score = 0
-valid = {}
-special_cases = ['Type: Null', 'Tapu Koko', 'Tapu Lele', 'Tapu Bulu', 'Tapu Fini', 'Mr. Mime', 'Mime Jr.', 'Nidoran M', 'Nidoran F', 'Mr. Rime', 'Great Tusk',
-                 'Brute Bonnet', 'Sandy Shocks', 'Scream Tail', 'Flutter Mane', 'Slither Wing', 'Roaring Moon', 'Iron Treads', 'Iron Moth', 'Iron Hands', 'Iron Jugulis',
-                 'Iron Thorns', 'Iron Bundle', 'Iron Valiant', 'Walking Wake', 'Iron Leaves']
-load = False
