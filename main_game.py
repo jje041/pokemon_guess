3c13e0c5af66880dd9ad4699a94b7bc5410fe5d5
jrn <jje041@post.uit.no>
2025-03-24 17:56:05 +0100

    Fixed ability bug.

--- main_game.py
+++ main_game.py
@@ -151,12 +151,12 @@ class MainGame:
                 self.game_api.show_pokemon_by_generation_and_type("guessing", gens)
             elif "info" in guess:
                 self.game_api.show_pokemon_info(guess)
+            elif "ability" in guess:
+                self.game_api.show_pokemon_by_ability("guessing", guess)
             elif "stats" in guess:
                 self.game_api.show_pokemon_stats(guess)
             elif "stat" in guess:
                 self.game_api.show_pokemon_by_stat("guessing", guess)
-            elif "ability" in guess:
-                self.game_api.show_pokemon_by_ability("guessing", guess)
             elif any(word in guess for word in keywords):
                 self.game_api.show_pokemon_by_generation_and_type("guessing", guess)
             elif guess == "save":
